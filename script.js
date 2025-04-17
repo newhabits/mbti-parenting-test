b40f34f3b86b86235b67ebf5a34c1f1f12c2c8d2
newhabits <newhabits@naver.com>
2025-04-17 14:03:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let scores = { E: 0, I: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0 };
 let previousAnswers = [];
 
 function startTest() {
+    alert("버튼이 클릭되었습니다!");  
     document.getElementById('intro-container').classList.add('hidden');
     document.getElementById('quiz-container').classList.remove('hidden');
     currentQuestionIndex = 0;
